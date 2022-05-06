33cbbd8a6196c537551812bac594ceda0818e983
Johorin <hhoyuak2145@gmail.com>
2022-05-06 15:04:46 +0900

    モデル（Bookinfo.php）に関数定義を記述しました。

--- app/Models/Bookinfo.php
+++ app/Models/Bookinfo.php
@@ -13,6 +13,52 @@ class Bookinfo extends Model
     protected $primaryKey = 'isbn';
     //マイグレーション時にテーブル名が'bookinfos'と認識されてしまっているため変更
     protected $table = 'bookinfo';
-    //timestamps関連のレコードは不要
+    //プライマリーキーがオートインクリメントではないため設定をオフ
+    public $incrementing = false;
+    //timestamps関連のレコードの自動更新をオフ
     public $timestamps = false;
+    
+    /*
+     * 関数名：selectByIsbn
+     * 引数：$isbn
+     * 戻り値：Collectionクラス
+     * 呼び出し元：書籍更新機能（UpdateController）
+     * 機能：引数のISBNを元にDBのbookinfoテーブルから該当書籍データの検索を行う。
+     */
+    public function selectByIsbn($isbn) {
+        return Bookinfo::where('isbn', '=', $isbn)->get();
+    }
+    
+    /*
+     * 関数名：insertBook
+     * 引数：$insertBook
+     * 戻り値：-
+     * 呼び出し元：書籍登録機能（InsertController）
+     * 機能：引数の書籍データを元にDBのbookinfoテーブルに新規登録処理を行う。
+     */
+    public function insertBook($insertBook) {
+        Bookinfo::insert($insertBook);
+    }
+    
+    /*
+     * 関数名：updateBook
+     * 引数：$isbn, $newBook
+     * 戻り値：-
+     * 呼び出し元：書籍更新機能（UpdateController）
+     * 機能：引数の書籍データを元にDBのbookinfoテーブルから該当書籍データの更新処理を行う。
+     */
+    public function updateBook($isbn, $newBook) {
+        Bookinfo::where('isbn', '=', $isbn)->update($newBook);
+    }
+    
+    /*
+     * 関数名：deleteBook
+     * 引数：$isbn
+     * 戻り値：-
+     * 呼び出し元：書籍削除機能（DeleteController）
+     * 機能：引数の書籍データ（ISBN）を元にDBのbookinfoテーブルから該当書籍データの削除処理を行う。
+     */
+    public function deleteBook($isbn) {
+        Bookinfo::where('isbn', '=', $isbn)->delete();
+    }
 }
