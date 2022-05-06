44e48ecd850d87327ce3f41452081417c0674c23
Johorin <hhoyuak2145@gmail.com>
2022-05-06 14:42:45 +0900

    ビューファイル（１つ前のコミット時には反映されていなかった）の作成、ルーティング処理をまとめて行いました。

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>メニュー画面</title>
+	</head>
+    <body>
+    <header>
+    	<h2 align="center">書籍販売システムWeb版 Ver.1.0</h2>
+    	<hr style="border: 2px solid blue;">
+    	<h3 align="center">MENU</h3>
+    	<hr style="border: 1px solid black;">
+    </header>
+    <main>
+		<center><br>
+			<p><a href="/list">【書籍一覧】</a></p>
+			<p><a href="/insert">【書籍登録】</a></p>
+		</center>
+    </main>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,65 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('hello', function () {
+//     return '<html><body><h1>Hello</h1><p>This is sample page.</p></body></html>';
+// });
+
+// Route::get('hello/{msg}', function ($msg) {
+//     $html = <<<EOF
+// EOF;
+// });
+
+//1.トップページ（メニュー画面）への遷移
+Route::get('/', function() {
+    view('menu');
 });
 
-Route::get('hello', function () {
-    return '<html><body><h1>Hello</h1><p>This is sample page.</p></body></html>';
+//2.書籍一覧画面への遷移
+Route::get('/list', 'ListController@index');
+
+//3.書籍登録画面（初期画面）への遷移
+Route::get('/insert', function() {
+    view('insert');
 });
 
-Route::get('hello/{msg}', function ($msg) {
-    $html = <<<EOF
-EOF;
+//4.書籍登録画面（登録ボタンから遷移）への遷移
+Route::post('/insert', 'InsertController@insert');
+
+//5.書籍登録画面へリダイレクト
+Route::get('/insert_redirect', function() {
+    redirect('/insert');
 });
+
+//6.書籍詳細画面への遷移
+Route::get('/detail', 'DetailController@index');
+
+//7.書籍更新画面（初期画面）への遷移
+Route::get('/update', 'UpdateController@index');
+
+//8.書籍更新画面（変更完了ボタンから遷移）への遷移
+Route::post('/update', 'UpdateController@update');
+
+//9.書籍削除画面への遷移
+Route::get('/delete', 'DeleteController@index');
+
+//10.書籍検索（結果）画面への遷移
+Route::post('/search', 'SearchController@index');
+
+//11.一覧画面へリダイレクト
+Route::get('/list_redirect', function() {
+    redirect('/list');
+});
+
+//12.エラー処理
+Route::get('/error', 'DeleteController@index');
+
+
+
+
+
+
