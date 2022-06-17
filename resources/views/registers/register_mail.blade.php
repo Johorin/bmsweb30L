fea4912da248aacede0a9efcc5ae4b691026a65a
Johorin <hoyuak2145@docomo.ne.jp>
2022-06-17 17:19:12 +0900

    書籍購入完了に自動送信するメールの本文の内容をリファクタリングしました。

--- resources/views/registers/register_mail.blade.php
+++ resources/views/registers/register_mail.blade.php
@@ -1,10 +1,10 @@
-{{ $userName }}様
-
-本のご購入ありがとうございます。
-以下内容でご注文を受け付けましたので、ご連絡致します。
-\n
+{{ $userName }}様<br>
+<br>
+本のご購入ありがとうございます。<br>
+以下内容でご注文を受け付けましたので、ご連絡致します。<br>
+<br>
 @foreach($cartInfo as $bookData)
-	{{ $bookData['isbn'] }} {{ $bookData['title'] }} {{ $bookData['price'] }}円\n
+	{{ $bookData['isbn'] }} {{ $bookData['title'] }} {{ $bookData['price'] }}円<br>
 @endforeach
-合計 {{ $total }}円\n\n
+合計 {{ $total }}円<br><br>
 またのご利用よろしくお願いします。
